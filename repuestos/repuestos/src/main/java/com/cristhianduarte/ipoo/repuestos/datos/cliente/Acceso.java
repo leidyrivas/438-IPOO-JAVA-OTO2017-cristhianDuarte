133220951475118c7c2be8ad23b4e9c19579798c
alumno-202 <alumno-202@M202-02.uaa.local>
2017-05-03 21:44:53 -0400

    Agregada la clase acceso

---
+++ repuestos/repuestos/src/main/java/com/cristhianduarte/ipoo/repuestos/datos/cliente/Acceso.java
@@ -0,0 +1,14 @@
+package com.cristhianduarte.ipoo.repuestos.datos.cliente;
+
+import java.util.Date;
+
+public class Acceso {
+		private Date fechaHoraInicio;
+		private Date fechaHoraFin;
+		private String ip;
+		private Cliente cliente;
+		
+		public Acceso(){
+			
+		}
+}
