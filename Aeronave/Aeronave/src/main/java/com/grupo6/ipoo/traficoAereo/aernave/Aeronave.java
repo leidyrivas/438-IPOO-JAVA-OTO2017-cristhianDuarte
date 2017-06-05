eba502014ee08943b2e23882940b27ea494029c1
Andres Lezcano <Andres Lezcano@AndresLezcano>
2017-06-05 16:22:08 -0400

    Prueba del metodo list con el atributo plan de vuelo

--- Aeronave/Aeronave/src/main/java/com/grupo6/ipoo/traficoAereo/aernave/Aeronave.java
+++ Aeronave/Aeronave/src/main/java/com/grupo6/ipoo/traficoAereo/aernave/Aeronave.java
@@ -1,5 +1,6 @@
 package com.grupo6.ipoo.traficoAereo.aernave;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Aeronave {
@@ -10,11 +11,61 @@ public class Aeronave {
 		private int tamTanque;
 		private int cantMotores;
 		private int canPersonaTripulacion;
-		private List<PlanDeVuelo> planDeVuelo; 
-		private List<TipoDeAvion> tipoDeAvion;
+		
+		//esto comento para poder hacer el list
+		//este es el original
+		//private List<PlanDeVuelo> planDeVuelo; 
+		//private List<TipoDeAvion> tipoDeAvion;
+		
+		
+		//este es de prueba
+		private List<Integer> planDeVuelo;
+		private List<Integer> tiposDeAvion;
+		
+		
+		
 		private Gps gps;
 		
 		
+		//DESDE ACA ME EMPECE A DIVAGAR |"|(>-<)|"|
+		//constructor divague1
+		public Aeronave(){
+			planDeVuelo=new ArrayList<Integer>();
+		}
 		
 		
+		
+		
+		//Este seria el LIST
+		public void testList(){
+			
+			int valor=0;
+			// tope
+			int valorMaximo=10;
+			
+			System.out.println("(Antes de asignar) Cuantos elementos tiene: " + planDeVuelo.size());
+			
+			int indice=0;
+			while(indice < valorMaximo){
+				valor++;
+				System.out.println("Asignando " + valor);
+				planDeVuelo.add(valor);
+				indice++;
+			}
+			
+			System.out.println("(Despues de asignar) Cuantos elementos tiene: " + planDeVuelo.size());
+			indice=0;
+			
+			while(indice<planDeVuelo.size()){
+				System.out.println(planDeVuelo.get(indice));
+				indice++;
+			}
+		
+		}
+		
+		public static void main(String[] args){
+			Aeronave pa=new Aeronave();
+			// pa.test();
+			pa.testList();
+		}
 }
